afb30e04c26b4890b1d4543a49ea814151edad70
Dracully <shlibardi@gmail.com>
2026-08-08 13:25:01 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
 // Desafio Super Trunfo - Países
-// Tema 2 - densidade populacional e o PIB per capita.
-// Objetivo: No nível intermediário você deve para calcular novas propriedades importantes para cada cidade: a densidade populacional e o PIB per capita.
-
+// Tema 3 - Batalha de Cartas no Super Trunfo.
+// Objetivo: exibir novas propriedadse e comparar cartas
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
   // Variáveis da Carta 1
   char estado[2], cod[4], cidade[15];
-  float area, prod_int, densidade, prod_int_capta;
-  int turismo, populacao;
+  float area, prod_int, densidade, prod_int_capta, superpoder;
+  int turismo;
+  unsigned long int populacao;
+  
   
   // Variáveis da Carta 2
   char estado2[2], cod2[4], cidade2[15];
-  float area2, prod_int2, densidade2, prod_int_capta2;
-  int turismo2, populacao2;
+  float area2, prod_int2, densidade2, prod_int_capta2, superpoder2;
+  int turismo2;
+  unsigned long int populacao2;
   
   
   
@@ -31,7 +33,7 @@ int main() {
   scanf("%s", &cidade);
 
   printf("Digite aqui a população da cidade:\n");
-  scanf("%d", &populacao);
+  scanf("%lu", &populacao);
 
   printf("Digite aqui a área da cidade:\n");
   scanf("%f", &area);
@@ -41,8 +43,6 @@ int main() {
 
   printf("Digite aqui o número de pontos turísticos:\n");
   scanf("%d", &turismo);
-
-
 
   // *Entrada de dados para a Carta 2*
   
@@ -57,7 +57,7 @@ int main() {
   scanf("%s", &cidade2);
 
   printf("Digite aqui a população da cidade:\n");
-  scanf("%d", &populacao2);
+  scanf("%lu", &populacao2);
 
   printf("Digite aqui a área da cidade:\n");
   scanf("%f", &area2);
@@ -71,13 +71,14 @@ int main() {
   // Cálculos Carta 1
   densidade = (float) populacao / area;
   prod_int_capta = (float) prod_int / populacao;
-
+  superpoder = (float)  populacao + area + prod_int + turismo + (1/densidade) + prod_int_capta; 
 
   // Cálculos Carta 2
   densidade2 = (float) populacao2 / area2;
   prod_int_capta2 = (float) prod_int2 / populacao2;
-
+  superpoder2 = (float)  populacao2 + area2 + prod_int2 + turismo2 + (1/densidade2) + prod_int_capta2; 
   
+ 
   // Área para exibição dos dados da cidade
   
   
@@ -86,21 +87,23 @@ int main() {
   "- Estado: %s \n"
   "- Código: %s \n"
   "- Nome da Cidade: %s \n"
-  "- População: %d \n"
+  "- População: %lu \n"
   "- Área: %f km² \n"
   "- PIB: %f bilhões de reais \n"
   "- Número de Pontos Turísticos: %d \n"
   "- Densidade Populacional: %f \n"
-  "- PIB per capita: %f \n \n",
+  "- PIB per capita: %f \n"
+  "- SUPERPODER: %f \n \n",
   estado, 
   cod, 
   cidade,
-  populacao, 
+  populacao,
   area, 
   prod_int, 
   turismo,
   densidade, 
-  prod_int_capta
+  prod_int_capta,
+  superpoder
   
   );
 
@@ -109,12 +112,13 @@ int main() {
   "- Estado: %s \n"
   "- Código: %s \n"
   "- Nome da Cidade: %s \n"
-  "- População: %d \n"
+  "- População: %lu \n"
   "- Área: %f km² \n"
   "- PIB: %f bilhões de reais \n"
   "- Número de Pontos Turísticos: %d \n"
   "- Densidade Populacional: %f \n"
-  "- PIB per capita: %f \n",
+  "- PIB per capita: %f \n"
+  "- SUPERPODER: %f \n \n",
   estado2, 
   cod2, 
   cidade2,
@@ -123,9 +127,26 @@ int main() {
   prod_int2, 
   turismo2,
   densidade2, 
-  prod_int_capta2
-  );
+  prod_int_capta2, 
+  superpoder2
+  
+);
+// comparações
+    printf("Resultado da Comparação Carta 1 com Carta 2 \n");
+    printf("Resposta 1 a carta 1 vence para 0 a carta 1 perde \n \n");
+    printf("População carta 1 venceu : %d\n", populacao < populacao2);
+    printf("Área carta 1 venceu : %d\n", area < area2);
+    printf("PIB carta 1 venceu: %d\n", prod_int < prod_int2);
+    printf("Número de pontos turísticos carta 1 venceu: %d\n", turismo < turismo2);
+    printf("Densidade Populacional carta 1 venceu: %d\n", densidade < densidade2);
+    printf("PIB per capta carta 1 venceu: %d\n", prod_int_capta <  prod_int_capta2);
+    printf("SUPERPODER carta 1 venceu: %d\n", superpoder <  superpoder2);
+
+    printf("Parabéns ao vencedor !!");
+
+
 return 0;
 } 
+
 
 
